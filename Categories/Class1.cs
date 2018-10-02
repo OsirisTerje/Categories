5da241b1b3576b0f943181e991eef0ef9edb48ee
Terje Sandstrom <terje@hermit.no>
2018-10-03 01:49:27 +0200

    Initial code

--- Categories/Class1.cs
+++ Categories/Class1.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Categories
 {
-    public class Class1
+    [Category("BigFeature")]
+    public class TheBigFeature
     {
+        [Test]
+        public void WhateverWeTest()
+        {
+            Assert.Pass();
+        }
+
+        [Category("SomethingElse")]
+        [Test]
+        public void SomethingElseTest()
+        {
+            Assert.Pass();
+        }
     }
 }
---
+++ Core/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Core
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello World!");
+    //    }
+    //}
+
+
+    [Category("BigFeature")]
+    public class TheCoreFeature
+    {
+        [Test]
+        public void WhateverWeTestInCore()
+        {
+            Assert.Pass();
+        }
+
+        [Category("SomethingElse")]
+        [Test]
+        public void SomethingElseTestInCore()
+        {
+            Assert.Pass();
+        }
+    }
+}
